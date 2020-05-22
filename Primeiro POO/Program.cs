120c0242025ac159921110488337340e7bfad735
MatheusBelloni <61584857+MatheusBelloni@users.noreply.github.com>
2020-05-22 10:39:38 -0300

    Add files via upload

---
+++ Primeiro POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primeiro_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            personagem Ichigo = new personagem();
+            Ichigo.espada = "zangetsu";
+
+            Console.WriteLine(Ichigo.nome);
+
+            Console.WriteLine(Ichigo.espada);
+
+            Console.WriteLine(Ichigo.reiryoku);
+
+            Console.WriteLine(Ichigo.reiatsu);
+
+            Console.WriteLine(Ichigo.Atacar());
+        }
+    }
+}
---
+++ Primeiro POO/personagem.cs
@@ -0,0 +1,23 @@
+namespace Primeiro_POO
+{
+    public class personagem
+    {
+        
+        public string nome = "Ichigo Kurosaki";
+
+        public int idade = 15;
+        
+        public string espada = "zangetsu";
+
+        public int reiryoku = 1000;
+
+        public int reiatsu = 600;
+
+        public string Atacar(string espada , int reiryoku , int reiatsu){
+            return "Ichigo atacou";
+
+        }
+        
+
+    }
+}
